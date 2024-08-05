91ad5af8e6e3621428473b4e3082bc96082edb85
Luciano Chiodini <138342951+yLucino@users.noreply.github.com>
2024-08-05 16:06:01 -0300

    Finish frontend in scheduling page (tsx functionality and layout responsive)

--- src/components/pages/scheduling/scheduling.tsx
+++ src/components/pages/scheduling/scheduling.tsx
@@ -4,25 +4,6 @@ import './scheduling-responsive.css';
 import React, { useState } from 'react';
 
 const Scheduling: React.FC = () => {
-  // function to add characters in phone number
-  const [phoneNumber, setPhoneNumber] = useState('');
-
-  const formatPhoneNumber = (value: string) => {
-    const cleaned = value.replace(/\D/g, '');
-    const match = cleaned.match(/^(\d{0,2})(\d{0,5})(\d{0,4})$/);
-    if (match) {
-      const formatted = `(${match[1]}${match[1] && match[2] ? ')' : ''} ${match[2]}${match[3] ? '-' : ''}${match[3]}`;
-      return formatted.trim();
-    }
-    
-    return value;
-  };
-
-  const handleChangePhoneNumber = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const formattedPhoneNumber = formatPhoneNumber(event.target.value);
-    setPhoneNumber(formattedPhoneNumber);
-  };
-
   // function to update min/max in date input
   function getMinDate() {
     const now = new Date();
@@ -53,7 +34,6 @@ const Scheduling: React.FC = () => {
         return dayOfYear
       } 
     } 
-    
   }
   
   function getMaxDate() {
@@ -64,11 +44,9 @@ const Scheduling: React.FC = () => {
     if (month < 10) {
       const monthLessThanTen = "0" + `${month}`
       const dayOfYear = `${year}-${monthLessThanTen}-${getLastDayOfMonth(year, month)}`      
-
       return dayOfYear
     } else {
       const dayOfYear = `${year}-${month}-${getLastDayOfMonth(year, month)}` 
-      
       return dayOfYear
     } 
   }
@@ -76,25 +54,25 @@ const Scheduling: React.FC = () => {
   function getLastDayOfMonth(year: number, month: number): number {
     const firstDayOfNextMonth = new Date(year, month, 1);
     firstDayOfNextMonth.setDate(firstDayOfNextMonth.getDate() - 1);
-    
     return firstDayOfNextMonth.getDate();
   }
 
-  // function for update min/max in hour input
+  // function for update min/max in hour input AND get day value input type date
   const daysOfWeek = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'];
   const daysNotWorking: string[] = ['Dom', 'Seg'];
   const hoursDayWorkingWeek: string[] = ['09:00', '19:30'];
   const hoursDayWorkingWeeked: string[] = ['08:00', '18:30'];
-
-  const [inputValue, setInputValue] = useState<string>('');
+  const [dateValue, setDateValue] = useState<string>('');
+  const [dateValueBR, setDateValueBR] = useState<string>('');
   const [day, setDay] = useState<number>(Number);
   const [month, setMonth] = useState<number>(Number);
   const [year, setYear] = useState<number>(Number);
-  const handleChangeGetMinHour = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChangeGetDate = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value;
-    setInputValue(value);
+    const formattedDate = formatDateToBR(value);
+    setDateValueBR(formattedDate);
+    setDateValue(value);
     const [year, month, day] = value.split('-').map(Number);
-
     setDay(day);
     setMonth(month);
     setYear(year);
@@ -113,7 +91,6 @@ const Scheduling: React.FC = () => {
   
   function getMinHour() {
     const dayName = getDayName(day, month, year);
-    
     if (dayName == daysNotWorking[0] || dayName == daysNotWorking[1]) {
       return '00:00'
     } else if (dayName == daysOfWeek[6]) {
@@ -125,7 +102,6 @@ const Scheduling: React.FC = () => {
   
   function getMaxHour() {
     const dayName = getDayName(day, month, year);
-
     if (dayName == daysNotWorking[0] || dayName == daysNotWorking[1]) {
       return '00:00'
     } else if (dayName == daysOfWeek[6]) {
@@ -134,6 +110,96 @@ const Scheduling: React.FC = () => {
       return hoursDayWorkingWeek[1]
     }
   }
+
+  // function to get => Full name in input type text
+  const [nameValue, setNameValue] = useState<string>('');
+  const [displayNameValue, setDisplayNameValue] = useState<string>('');
+  const handleChangeGetName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setNameValue(value);    
+  }
+
+  // function to get => E-mail in input type e-mail
+  const [emailValue, setEmailValue] = useState<string>('');
+  const [displayEmailValue, setDisplayEmailValue] = useState<string>('');
+  
+  const handleChangeGetEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setEmailValue(value);    
+  }
+
+  // function to get => Number of phone in input type number
+  const [phoneValue, setPhoneValue] = useState<string>('');
+  const [displayPhoneValue, setDisplayPhoneValue] = useState<string>('');
+
+  const formatPhoneNumber = (value: string) => {
+    const cleaned = value.replace(/\D/g, '');
+    const match = cleaned.match(/^(\d{0,2})(\d{0,5})(\d{0,4})$/);
+    if (match) {
+      const formatted = `(${match[1]}${match[1] && match[2] ? ')' : ''} ${match[2]}${match[3] ? '-' : ''}${match[3]}`;
+      return formatted.trim();
+    }
+    return value;
+  };
+
+  const handleChangeGetPhone = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const formattedPhoneNumber = formatPhoneNumber(event.target.value);
+    setPhoneValue(formattedPhoneNumber);
+  }
+
+  // function to get => Response of input checked in 'monthly plan', type radio
+  const [planRadioValue, setPlanRadioValue] = useState<string>('');
+  const [displayPlanRadioValue, setDisplayPlanRadioValue] = useState<string>('');
+  
+  const handleChangeGetPlanRadio = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setPlanRadioValue(value);    
+  }
+
+  // function to get/formatted in Br => Full date in input type date
+  const [displayDateValue, setDisplayDateValue] = useState<string>('');
+  const formatDateToBR = (dateString: string): string => {
+    const [year, month, day] = dateString.split('-');
+    return `${day}/${month}/${year}`;
+  };
+
+  // function to get => Full hour in input type hour
+  const [hourValue, setHourValue] = useState<string>('');
+  const [displayHourValue, setDisplayHourValue] = useState<string>('');
+  
+  const handleChangeGetHour = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setHourValue(value);    
+  }
+
+  // function to pass info in form to CheckOut box
+  function formConcluded() {
+    setDisplayNameValue(nameValue);
+    setDisplayDateValue(dateValueBR);
+    setDisplayEmailValue(emailValue);
+    setDisplayPhoneValue(phoneValue);
+    setDisplayPlanRadioValue(planRadioValue);
+    setDisplayHourValue(hourValue);
+  }
+
+  // function to reset all values in form scheduling and checkout
+  const formCancel = () => {
+    // Form Scheduling Inputs
+    setNameValue('');
+    setDateValue('');
+    setEmailValue('');
+    setPhoneValue('');
+    setPlanRadioValue('');
+    setHourValue('');
+    // Form Checkout Info
+    setDisplayNameValue('');
+    setDisplayDateValue('');
+    setDisplayEmailValue('');
+    setDisplayPhoneValue('');
+    setDisplayPlanRadioValue('');
+    setDisplayHourValue('');
+  };
+
 
   return (
     <>
@@ -152,19 +218,20 @@ const Scheduling: React.FC = () => {
                 <form action="URL" method="post" id="form-scheduling">
                   <div className="box-user-info">
                     <label htmlFor="name">Nome Completo: </label><br></br>
-                    <input type="text" id="name" name="name" required/><br></br>
+                    <input type="text" id="name" name="name" value={nameValue} onChange={handleChangeGetName} maxLength={40} required/><br></br>
                     <label htmlFor="email">E-mail:</label><br></br>
-                    <input type="email" name="email" id="email" placeholder='email@exemplo.com' required/><br></br>
+                    <input type="email" name="email" id="email" placeholder='email@exemplo.com' value={emailValue} onChange={handleChangeGetEmail} maxLength={40} required/><br></br>
                     <label htmlFor="phone">Telefone: </label><br></br>
-                    <input type="text" name="phone" id="phone" placeholder='(xx) xxxxx-xxxx' value={phoneNumber} onChange={handleChangePhoneNumber} maxLength={15} required/><br></br>
+                    <input type="text" name="phone" id="phone" placeholder='(xx) xxxxx-xxxx' value={phoneValue} onChange={handleChangeGetPhone} maxLength={15} required/><br></br>
                   </div>
                   <div className="box-request-subscriber-plan">
                     <label htmlFor="subscriber">Assinante do plano mensal? </label><br></br>
                     <div className="radio-container">
-                      <input type="radio" name="subscriber" id="yes" value="yes" required/>
+                      <input type="radio" name="subscriber" id="yes" value='Sim' checked={planRadioValue === 'Sim'} onChange={handleChangeGetPlanRadio} required/>
                       <div className='custom-radio'><span></span></div>
                       <label className='label-yes-no' htmlFor="yes">Sim! </label>
-                      <input type="radio" name="subscriber" id="no" value="no" required/>
+
+                      <input type="radio" name="subscriber" id="no" value='Não' checked={planRadioValue === 'Não'} onChange={handleChangeGetPlanRadio} required/>
                       <div className='custom-radio'><span></span></div>
                       <label className='label-yes-no' htmlFor="no">Não! </label><br></br>
                     </div>
@@ -172,10 +239,9 @@ const Scheduling: React.FC = () => {
                   <div className="box-day-hour">
                     <div className="day-hour">
                       <label htmlFor="date">Data: </label>
-                      <input type="date" name="date" id="date" max={getMaxDate()} min={getMinDate()} value={inputValue} onChange={handleChangeGetMinHour} required/><br></br>
+                      <input type="date" name="date" id="date" max={getMaxDate()} min={getMinDate()} value={dateValue} onChange={handleChangeGetDate} required/><br></br>
                       <label htmlFor="time">Hora: </label>
-                      <input type="time" name="time" id="time" min={getMinHour()} max={getMaxHour()} required onClick={getHour}/>
-                    {/* Fazer com que o maxtime/mintime se atualize automaticamento */}
+                      <input type="time" name="time" id="time" min={getMinHour()} max={getMaxHour()} value={hourValue} onChange={handleChangeGetHour} onClick={getHour} required />
                       <span className="validacao"></span>
                     </div>
                     <p>(Ter a Sex: 09:00h as 19:30h | Sáb: 08:00h as 18:30h)</p>
@@ -204,6 +270,36 @@ const Scheduling: React.FC = () => {
                           <p>R$ 30</p>
                         </div>
                       </li>
+                      <li className='service'>
+                        <div className="info-service">
+                          <button>
+                            <i className='bx bxs-trash'></i>
+                          </button>
+                          <img src="https://placehold.co/50x50" />
+                          <div className="text">
+                            <p className='title-text'>Corte de Cabelo</p>
+                            <p className='sub-text'>Preço fora da assinatura</p>
+                          </div>
+                        </div>
+                        <div className='price-service'>
+                          <p>R$ 30</p>
+                        </div>
+                      </li>
+                      <li className='service'>
+                        <div className="info-service">
+                          <button>
+                            <i className='bx bxs-trash'></i>
+                          </button>
+                          <img src="https://placehold.co/50x50" />
+                          <div className="text">
+                            <p className='title-text'>Corte de Cabelo</p>
+                            <p className='sub-text'>Preço fora da assinatura</p>
+                          </div>
+                        </div>
+                        <div className='price-service'>
+                          <p>R$ 30</p>
+                        </div>
+                      </li>
                     </ul>
                   </div>
                   <div className="options-services-select">
@@ -211,9 +307,8 @@ const Scheduling: React.FC = () => {
                   </div>
                 </div>
                 <div className="finish-box-scheduling">
-                  <button className='concluded'>Concluído</button>
-                  <button className='cancel'>Cancelar</button>
-                  {/* chamar function para enviar as informações do form para o CheckOut */}
+                  <button className='concluded' onClick={formConcluded}>Concluído</button>
+                  <button className='cancel' onClick={formCancel}>Cancelar</button>
                 </div>
               </div>
             </div>
@@ -243,6 +338,30 @@ const Scheduling: React.FC = () => {
                     <p>R$ 30</p>
                   </div>
                 </li>
+                <li className='service'>
+                  <div className="info-service">
+                    <img src="https://placehold.co/50x50" />
+                    <div className="text">
+                      <p className="title-text">Corte de Cabelo</p>
+                      <p className="sub-text">Preço fora da assinatura</p>
+                    </div>
+                  </div>
+                  <div className='price-service'>
+                    <p>R$ 30</p>
+                  </div>
+                </li>
+                <li className='service'>
+                  <div className="info-service">
+                    <img src="https://placehold.co/50x50" />
+                    <div className="text">
+                      <p className="title-text">Corte de Cabelo</p>
+                      <p className="sub-text">Preço fora da assinatura</p>
+                    </div>
+                  </div>
+                  <div className='price-service'>
+                    <p>R$ 30</p>
+                  </div>
+                </li>
               </ul>
             </div>
             <div className="box-info-scheduling">
@@ -251,14 +370,14 @@ const Scheduling: React.FC = () => {
               </div>
               <div className="container-info">
                 <div className="info-left">
-                  <p>Nome: <span>Jonh Sera</span></p>
-                  <p>E-mail: <span>jonh.sera@gmail.com</span></p>
-                  <p>Telefone: <span>(47) 99248-6476</span></p>
+                  <p>Nome: <span>{displayNameValue}</span></p>
+                  <p>E-mail: <span>{displayEmailValue}</span></p>
+                  <p>Telefone: <span>{displayPhoneValue}</span></p>
                 </div>
                 <div className="info-right">
-                  <p>É assinante: <span>Não</span></p>
-                  <p>Data: <span>01/01/2024</span></p>
-                  <p>Hora: <span>20:30</span></p>
+                  <p>É assinante: <span>{displayPlanRadioValue}</span></p>
+                  <p>Data: <span>{displayDateValue}</span></p>
+                  <p>Hora: <span>{displayHourValue}</span></p>
                 </div>
               </div>
             </div>
